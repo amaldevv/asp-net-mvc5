0912275e91363f5735255f56e961bfe3c0912af1
Amal <amaldevv@outlook.com>
2015-04-27 09:23:28 +0530

    added hello mvc project

---
+++ Projects/Hello MVC/Hello MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello_MVC.Startup))]
+namespace Hello_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
